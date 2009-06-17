2e3917fda5a4094b3d8716dc123a8c2c8d7bebc5
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2009-06-17 14:05:42 +0000

    fixes for jets and met

--- test/l1Val_cfg.py
+++ test/l1Val_cfg.py
@@ -3,8 +3,8 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("l1validation")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.load("L1Trigger.Configuration.L1DummyConfig_cff")
-process.load("L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu2008_2E30_Unprescaled_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string("IDEAL_31X::All")
 
 process.load("L1Trigger.Configuration.L1Extra_cff")
 process.l1extraParticles.muonSource = cms.InputTag("hltGtDigis")
@@ -19,24 +19,38 @@ process.l1extraParticles.etMissSource = cms.InputTag("hltGctDigis")
 
 process.load("L1TriggerOffline.L1Analyzer.L1MuonMCAnalysis_cff")
 
+process.load("L1TriggerOffline.L1Analyzer.L1EmMCAnalysis_cff")
 process.load("L1TriggerOffline.L1Analyzer.L1IsoEmMCAnalysis_cff")
-
 process.load("L1TriggerOffline.L1Analyzer.L1NonIsoEmMCAnalysis_cff")
 
-process.load("L1TriggerOffline.L1Analyzer.L1IsoEmRecoAnalysis_cff")
-
-process.load("L1TriggerOffline.L1Analyzer.L1NonIsoEmRecoAnalysis_cff")
-
+process.load("L1TriggerOffline.L1Analyzer.L1JetMCAnalysis_cff")
 process.load("L1TriggerOffline.L1Analyzer.L1CenJetMCAnalysis_cff")
-
-process.load("L1TriggerOffline.L1Analyzer.L1TauJetMCAnalysis_cff")
-
 process.load("L1TriggerOffline.L1Analyzer.L1ForJetMCAnalysis_cff")
+process.load("L1TriggerOffline.L1Analyzer.L1TauJetMCAnalysis_cff")
 
 process.load("L1TriggerOffline.L1Analyzer.L1MetMCAnalysis_cff")
 
+#process.load("L1TriggerOffline.L1Analyzer.L1IsoEmRecoAnalysis_cff")
+#process.load("L1TriggerOffline.L1Analyzer.L1NonIsoEmRecoAnalysis_cff")
+
+
 process.load("L1Trigger.GlobalTriggerAnalyzer.l1GtTrigReport_cfi")
 process.l1GtTrigReport.L1GtRecordInputTag = cms.InputTag("hltGtDigis")
+
+process.p = cms.Path(
+                     process.L1MuonMCAnalysis
+                     +process.L1EmMCAnalysis
+                     +process.L1IsoEmMCAnalysis
+                     +process.L1NonIsoEmMCAnalysis
+                     +process.L1JetMCAnalysis
+                     +process.L1CenJetMCAnalysis
+                     +process.L1ForJetMCAnalysis
+                     +process.L1TauJetMCAnalysis
+                     +process.L1MetMCAnalysis)
+
+process.e = cms.EndPath(process.l1GtTrigReport)
+process.TFileService.fileName = 'l1Val.root'
+
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:TTbar_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root')
 )
@@ -44,7 +58,4 @@ process.source = cms.Source("PoolSource",
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1000)
 )
-process.p = cms.Path(process.l1extraParticles+process.L1MuonMCAnalysis+process.L1IsoEmMCAnalysis+process.L1NonIsoEmMCAnalysis+process.L1CenJetMCAnalysis+process.L1ForJetMCAnalysis+process.L1TauJetMCAnalysis+process.L1MetMCAnalysis)
-process.e = cms.EndPath(process.l1GtTrigReport)
-process.TFileService.fileName = 'l1Val.root'
 
