ecfef60e50cd26932d85737c7320bc61c9bda78c
Vasile Mihai Ghete <sha1-bda472ebd3845b96039bd3194f2d7659260e27b4@cern.ch>
2011-06-15 12:38:38 +0000

    Removed L1Analyzer (no more maintained) - to be replaced by the DQM packages Validation/L1Trigger and DQMOffline/L1Trigger and by L1PromptAnalysis.

--- test/l1Val_IDEAL_cfg.py
+++
@@ -1,3 +0,0 @@
-from L1TriggerOffline.Configuration.l1Val_cfg import *
-
-process.GlobalTag.globaltag = cms.string("IDEAL_31X::All")
--- test/l1Val_STARTUP_cfg.py
+++
@@ -1,3 +0,0 @@
-from L1TriggerOffline.Configuration.test.l1Val_cfg import *
-
-process.GlobalTag.globaltag = cms.string("STARTUP_31X::All")
